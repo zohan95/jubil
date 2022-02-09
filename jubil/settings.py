fda3e2e7e82732003291f7d4ea2eb9c0ee7e090a
zohan95 <zalkar_95@mail.ru>
2022-02-09 21:56:18 +0600

    Update settings.py

--- jubil/settings.py
+++ jubil/settings.py
@@ -138,7 +138,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 #*****************
 
