bf16c347755562a6e112d6de8373fc3b08834378
zohan95 <zalkar_95@mail.ru>
2022-02-11 00:58:44 +0600

    upload full worked

--- viewcards/urls.py
+++ viewcards/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.cardshower, name='cardshower'),
+    path('', views.mainmenu, name='mainmenu'),
     path('upload/', views.Upload.as_view(), name='upload_url'),
+    path('viewid/<str:course>/<str:category>', views.ViewById.as_view(), name='view_id_url'),
 ]
--- viewcards/views.py
+++ viewcards/views.py
@@ -1,41 +1,83 @@
+from unicodedata import category
 from django.shortcuts import render
 from django.views import View
 from .models import *
 import openpyxl
 
 
-def cardshower(request):
-    return render(request, 'viewcards/main.html',{'title':'Карточки из документа'})
+# def cardshower(request):
+#     courses = CourseJubil.objects.all()
+#     categorys = WordsCategory.objects.all()
+#     tmp = {}
+#     for i in courses:
+#         tmp1 = []
+#         tmp_obj = WordsCategory.objects.filter(course_jubil=i.id)
+#         tmp1 = [k.words_category for k in tmp_obj]
+#         #print(tmp1)
+#         tmp[i.course_jubil]=tmp1
+#     #print(tmp)
+#     return render(request, 'viewcards/main.html',{'title':'Карточки из документа', 'courses': courses, 'categorys':categorys,'catdict':tmp})
 
+def mainmenu(request):
+    courses = CourseJubil.objects.all()
+    categorys = WordsCategory.objects.all()
+    tmp = {}
+    for i in courses:
+        tmp1 = []
+        tmp_obj = WordsCategory.objects.filter(course_jubil=i.id)
+        tmp1 = [k.words_category for k in tmp_obj]
+        #print(tmp1)
+        tmp[i.course_jubil]=tmp1
+    #print(tmp)
+    return render(request, 'viewcards/base.html',{'title':'Карточки из документа', 'courses': courses, 'categorys':categorys,'catdict':tmp})
 
 
 class Upload(View):
     def get(self, request):
-        return render(request, 'viewcards/upload.html')
+        courses = CourseJubil.objects.all()
+        categorys = WordsCategory.objects.all()
+        return render(request, 'viewcards/upload.html',{'course':courses, 'category':categorys})
     
     def post(self, request):
         excel_file = request.FILES['files']
-        # response = HttpResponse(
-        #     content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
-        # )
-        # response['Content-Disposition'] = 'attachment; filename={date}-{id}.xlsx'.format(
-        #     date=datetime.datetime.now().strftime('%Y-%m-%d'), id=randint(1,10000000)
-        # )
+        interest = request.POST.get('course')
+        toto = request.POST.get('category')
 
-        # wb2 = openpyxl.Workbook()
-        # ws = wb2.active
         wb = openpyxl.load_workbook(excel_file)
         worksheet = wb.active
         for i in range(0, worksheet.max_row):
             post = WordsContainer(
                 rus_word = worksheet['A'+str(i+1)].value,
                 end_word = worksheet['B'+str(i+1)].value,
-                words_category = WordsCategory.objects.get(words_category='Из документа')
+                words_category = WordsCategory.objects.get(words_category=toto)
             )
             post.save()
 
+        
+        #print(interest, toto)
 
-
-        # wb2.save(response)
+        
         wb.close()
         return render(request, 'viewcards/base.html')
+
+
+class ViewById(View):
+    def get(self, request, course, category):
+        courses = CourseJubil.objects.all()
+        categorys = WordsCategory.objects.all()
+        tmp2 = {}
+        for i in courses:
+            tmp1 = []
+            tmp_obj = WordsCategory.objects.filter(course_jubil=i.id)
+            tmp1 = [k.words_category for k in tmp_obj]
+            #print(tmp1)
+            tmp2[i.course_jubil]=tmp1
+        #request.GET.get('course')
+        print(category)
+        lol = WordsCategory.objects.get(words_category=category)
+        tmp = WordsContainer.objects.filter(words_category=lol)
+        tmp_dict = []
+        for i in tmp:
+            tmp_dict.append([i.rus_word, i.end_word])
+        tmp_words = [i.rus_word for i in tmp]
+        return render(request, 'viewcards/main.html', {'words':tmp_words,'translates':tmp_dict,'title':'Карточки из документа', 'courses': courses, 'categorys':categorys,'catdict':tmp2})
